b93375268692bde99ad93f9387ef2e77a1d11286
omn0mn0m <tranngocnam97@yahoo.com>
2018-02-26 15:48:48 -0500

    Added code for lift switch

--- src/org/usfirst/frc/team6584/robot/Robot.java
+++ src/org/usfirst/frc/team6584/robot/Robot.java
@@ -66,7 +66,7 @@ public class Robot extends IterativeRobot {
 	     SmartDashboard.putData("Reset Gyro", new ResetGyro());
 	        		
 		 CameraServer.getInstance().startAutomaticCapture(0);
-	  // CameraServer.getInstance().startAutomaticCapture(1);
+		 CameraServer.getInstance().startAutomaticCapture(1);
 		
 	}
 	/**
